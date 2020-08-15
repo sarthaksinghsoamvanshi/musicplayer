336c6cfd30b457f7a5626bae1be210474434e885
sarthak singh <sarthakmno@gmail.com>
2020-08-14 23:42:02 -0700

    After Appbar

--- lib/main.dart
+++ lib/main.dart
@@ -8,16 +8,45 @@ void main() {
 }
 
 class HomePage extends StatelessWidget {
+  Widget appbar()
+  {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        IconButton(icon: Icon(Icons.menu,color: Colors.white,), onPressed: null),
+        Text("Music Player",style: TextStyle(color:Colors.white,fontSize:25),),
+        IconButton(icon: Icon(Icons.search,color:Colors.white), onPressed: null)
+      ],
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: Stack(
-          children: [Container(
-            decoration:BoxDecoration(
-              gradient:LinearGradient(colors: [Colors.purple,Colors.blue,Colors.black])
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                // backgroundBlendMode: BlendMode.difference,
+                gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  tileMode: TileMode.mirror,
+                  colors: [
+                    Colors.purple,
+                    Colors.blue,
+                    Colors.black,
+                    Colors.black
+                  ],
+                ),
+              ),
+            ),
+            ListView(
+              children: [
+                appbar(),
+              ],
             )
-          )],
+          ],
         ),
       ),
     );
